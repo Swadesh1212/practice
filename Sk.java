9555d8667592d09ee620a8a3a6a9748ec90d1411
Swadesh <ankitaray1312@gmail.com>
2024-08-02 14:22:05 +0530

    modifing the same massage

--- Sk.java
+++ Sk.java
@@ -1,6 +1,6 @@
 public class Sk{
     public static void main(String... args){
         System.out.println("sk");
-        System.out.println("modifing it ");
+        System.out.println("modifing it but massage is same");
     }
 }
